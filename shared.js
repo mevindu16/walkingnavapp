bd7919f27990bc3f682a75b9c0d15cfda675e1a7
Mevindu Wickramasinghe <mevindu1998@gmail.com>
2018-05-16 17:03:41 +0000

    shared.js created online with Bitbucket

---
+++ shared.js
@@ -0,0 +1,8 @@
+'use strict';
+// Shared code needed by all pages of the app.
+
+// Prefix to use for Local Storage.  You may change this.
+var APP_PREFIX = "monash.eng1003.navigationApp";
+
+// Array of saved Path objects.
+var availablePaths = [];
