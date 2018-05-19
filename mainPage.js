b34bad411d661643a06209622e79308b0feec8b7
Mevindu Wickramasinghe <mevindu1998@gmail.com>
2018-05-19 15:05:16 +0000

    generating a list of available paths with the name of the location, distance and number of turns

--- mainPage.js
+++ mainPage.js
@@ -12,3 +12,20 @@ function viewPath(pathIndex)
     // ... and then load the Navigate page.
     location.href = 'navigate.html';
 }
+
+
+// obtaing paths from the local storage 
+retrievePaths();
+
+// initializing the HTML outputs
+var listOutput ="";
+
+//Generate a list of paths with title and distance and number of turns as a subtitle    
+for (var i = 1; i <= listOfPaths.length; i++){
+        listOutput += "<li class = \"mdl-list__item mdl-list__item--two-line\"" + "onclick = \"viewPath(" + i + ")\">"
+        listOutput += "<span class = \"mdl-list__item-primary-content\">" +"<span>" + listOfPaths[i-1].title + "</span>"
+        listOutput += "<span class=\"mdl-list__item-sub-title\">Distance: " + listOfPaths[i-1].distance.toFixed(1) + " m  Turns:"+listOfPaths[i-1].turns+"</span></span></li>"
+}
+
+//Adding code to HTML page
+document.getElementById("pathsList").innerHTML = listOutput;
