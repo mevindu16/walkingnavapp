27585b4559627a8044a79fc3990acfd98d0cc8e7
Mevindu Wickramasinghe <mevindu1998@gmail.com>
2018-05-19 15:24:00 +0000

    showing the title of the path on the header when user selects a specific path

--- navigatePage.js
+++ navigatePage.js
@@ -5,11 +5,11 @@
 // app.  You need can replace this in your final app.
 
 var pathIndex = localStorage.getItem(APP_PREFIX + "-selectedPath");
-if (pathIndex !== null)
-{
-    // If a path index was specified, show name in header bar title. This
-    // is just to demonstrate navigation.  You should set the page header bar
-    // title to an appropriate description of the path being navigated
-    var pathNames = [ "Path A", "Path B" ];
-    document.getElementById("headerBarTitle").textContent = pathNames[pathIndex];
+
+if (pathIndex !== null){
+    // the header bar shows the name of the path that the user has clicked on
+    document.getElementById("headerBarTitle").textContent = pathNames[pathIndex-1];
 }
+
+
+
